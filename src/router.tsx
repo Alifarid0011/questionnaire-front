33d79b821c15016a69620b81358d7c8669cffa9e
Ali Farid <alifarid0011@gmail.com>
2025-09-25 15:54:37 +0330

    feat: wrap routes with AppProvider

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { AppProvider } from "./providers";
 
 const Router = () => {
-  const router = createBrowserRouter([{}]);
+  const router = createBrowserRouter([
+    {
+      element: <AppProvider />,
+    },
+  ]);
 
   return <RouterProvider router={router} />;
 };
